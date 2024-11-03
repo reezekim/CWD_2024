70943258cc5610d9389b404848de00236269f6b1
reezeKim <reezekim.kr@gmail.com>
2024-11-03 20:24:14 +0900

    [add] Implement slides images. Tab menu, pop-up and menu nav

--- pages/B1_대한은행/js/script.js
+++ pages/B1_대한은행/js/script.js
@@ -1,6 +1,40 @@
 $(function () {
   // 슬라이드
-  // 팝업창
+  var i = 0;
+  function slide() {
+    if (i < 2) {
+      i++;
+    } else {
+      i = 0;
+    }
+    $('#slider ul').animate({ left: 1200 * -1 * i }, 1000);
+  }
+  setInterval(slide, 2000);
+
   // 탭메뉴
-  // 메뉴바
+  $('.tabmenu li').click(function () {
+    $('.tabmenu li').removeClass('on');
+    $(this).addClass('on');
+
+    var t = $(this).index();
+    $('.tabcon').hide();
+    $('.tabcon').eq(t).show();
+  });
+
+  // 팝업창
+  $('.pp').click(function () {
+    $('.popup').show();
+    return false;
+  });
+  $('.popup button').click(function () {
+    $('.popup').hide();
+  });
+
+  // 메뉴네비
+  $('nav>ul>li').mouseenter(function () {
+    $('.sub').stop().slideDown();
+  });
+  $('nav>ul>li').mouseleave(function () {
+    $('.sub').stop().slideUp();
+  });
 });
